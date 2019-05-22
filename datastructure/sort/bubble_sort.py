d7b005d02d5a693027d39bb4f36bbc23aedefd39
liuchang <598306303@qq.com>
2019-05-22 14:53:17 +0800

    double sort work

--- datastructure/sort/bubble_sort.py
+++ datastructure/sort/bubble_sort.py
@@ -3,9 +3,13 @@ def bubble_sort(alist):
     """ bubble sort """
     n = len(alist)
     for j in range(n - 1):
+        count = 0
         for i in range(0, n - 1 - j):
             if alist[i] > alist[i + 1]:
                 alist[i], alist[i + 1] = alist[i + 1], alist[i]
+                count += 1
+        if 0 == count:  # 改进之后最优时间复杂度为 0（n）最坏不变为0（n^2）
+            return
 
 
 if __name__ == "__main__":
