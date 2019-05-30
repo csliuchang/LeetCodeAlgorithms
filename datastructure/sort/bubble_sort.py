c37b92cf3caa9781dd1519cdad8116395cada028
liuchang <598306303@qq.com>
2019-05-30 16:34:13 +0800

    modify the bubble sort

--- datastructure/sort/bubble_sort.py
+++ datastructure/sort/bubble_sort.py
@@ -4,15 +4,15 @@ from __future__ import print_function
 
 def bubble_sort(alist):
     """ bubble sort """
-    n = len(alist)
-    for j in range(n - 1):
+    for j in range(len(alist) - 1):
         count = 0
-        for i in range(0, n - 1 - j):
+        for i in range(0, len(alist) - 1 - j):
             if alist[i] > alist[i + 1]:
                 alist[i], alist[i + 1] = alist[i + 1], alist[i]
                 count += 1
         if 0 == count:  # 改进之后最优时间复杂度为 0（n）最坏不变为0（n^2）
-            return
+            break
+    return alist
 
 
 if __name__ == '__main__':
