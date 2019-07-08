067a7c06ebc5c403d9ec5d2b47a0c1772a7f9e10
csliuchang <598306303@qq.com>
2019-07-08 10:02:15 -0400

    submit binary tree

---
+++ datastructure/binary_tree/binary_tree.py
@@ -0,0 +1,62 @@
+# coding:utf-8
+
+
+class Node(object):
+    """
+
+
+    """
+    def __init__(self,item):
+        self.elem = item
+        self.lchild = None
+        self.rchild = None
+
+
+class Tree(object):
+    def __init__(self):
+        self.root = None
+
+    def add(self, item):
+        node = Node(item)
+        if self.root is None:
+            self.root = node
+        queue = [self.root]
+        while queue:
+            cur_node = queue.pop(0)
+            if cur_node.lchild is None:
+                cur_node.lchild = node
+                return
+            else:
+                queue.append(cur_node.lchild)
+            if cur_node.rchild is None:
+                cur_node.rchild = node
+                return
+            else:
+                queue.append(cur_node.rchild)
+
+    def breadth_travel(self):
+        """
+        breadth travel
+        :return:
+        """
+        if self.root is None:
+            return
+        queue = [self.root]
+        while queue:
+            cur_node = queue.pop(0)
+            print(cur_node.elem)
+            if cur_node.lchild is not None:
+                queue.append(cur_node.lchild)
+            if cur_node.rchild is not None:
+                queue.append(cur_node.rchild)
+
+
+if __name__ == "__main__":
+    tree = Tree()
+    tree.add(1)
+    tree.add(2)
+    tree.add(3)
+    tree.add(4)
+    tree.add(5)
+    tree.add(6)
+    tree.breadth_travel()
