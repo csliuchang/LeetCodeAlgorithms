7c954a8a5fb9dc76cc57b49c1885fa9d043d582c
csliuchang <598306303@qq.com>
2019-07-07 23:15:15 -0400

    submit quick sort

--- datastructure/sort/quick_sort.py
+++ datastructure/sort/quick_sort.py
@@ -9,4 +9,9 @@ def quick_sort(alist):
         pivot =  alist[0]
         greater = [i for i in alist[1:] if i >pivot]
         lesser = [i for i in alist[1:] if i <= pivot]
-        return quick_sort(lesser) + [pivot]
+        return quick_sort(lesser) + [pivot] + quick_sort(greater)
+
+
+if __name__ == "__main__":
+    l1 = [12,45,67,89,90,54,56,23]
+    print(quick_sort(l1))
